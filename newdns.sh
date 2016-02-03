a2e7b840d97fa4f9a2b1ba0e17ca95552c994a32
Jasper Nuyens <jnuyens@users.noreply.github.com>
2016-02-03 17:18:04 +0100

    serverside script

---
+++ newdns.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#server side script
+#called as /newdns.sh location ip zonefile
+
+cd /etc/bind/zone/master
+name=$1
+ip=$2
+
+if [ "$ip" = "" ]
+then
+ exit
+fi
+
+oldip=`grep $name zone.linuxbe.com | awk '{ print $5 }'`
+sed -i s/$oldip/$ip/ $3
+
+serial=$(grep serial $3 | awk '{ print $1 }')
+newserial=$(($serial+1))
+sed -i s/$serial/$newserial/  $3
+
+/etc/init.d/bind9 restart
